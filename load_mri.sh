dbad9fce173668a4065c7cc87f1da97382d0aa77
DavidBrang <dbrang@Psytechs-Mac-Pro.local>
2011-04-22 12:56:08 -0700

    fix output directory problems added data label again

--- load_mri.sh
+++ load_mri.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env tcsh
 
-if ($#argv != 2) then
-  echo "Usage: $0 source_directory data_label"
+if ($#argv != 3) then
+  echo "Usage: $0 source_directory output_directory data_label"
   echo "runs MRI data in source_directory through Freesurfer recon"
-  echo "and creates a subject with labeled data_label"
+  echo "and creates a subject with labeled data_label in output_directory"
   goto error
 endif
 
 set source_directory=`dirname $1`
 set destination_directory=$2
-set label=`basename $1`
-
+set label=$3 
 
 echo "cd $destination_directory"
 echo "mksubjdirs $label"
@@ -28,17 +27,16 @@ cd $1
 
 cd *
 
-foreach path (*.$1)
-#count files
-set $file_count = `ls -1 $path | wc -l`
-if ($file_count >= 150 && $file_count <= 200) then
- set one_file = $path/*.1
- echo "convert $one_file"
-endif
+foreach directory (*)
+  set file_count = `ls -1 $directory | wc -l`
+  if ($file_count >= 150 && $file_count <= 200) then
+    set one_file = $directory/*.1
+    echo "mri_convert $one_file  $destination_directory$label/mri/orig/001.mgz"
+  endif
 end
 
 
-echo "stat $destination_directory/$label/mri/orig/001.mgz"
+echo "stat $destination_directory$label/mri/orig/001.mgz"
 
 echo "recon-all -subjid $label -all"
 
